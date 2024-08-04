f4b86875d01e12ffa31249d2f9b2367f32334b17
Aniket43210 <150520607+Aniket43210@users.noreply.github.com>
2024-08-04 15:23:08 +0000

    modified:   a2_05.py 	new file:   a2_06.py 	deleted:    gcd.py

--- a2_05.py
+++ a2_05.py
@@ -1,6 +1,20 @@
-i = int(input("Enter any number greater than 2:  "))
-p=2
-while p<=i:
-    print(1/p)
-    p=p+1
-    
+p = int(input("Enter a number:  "))
+i = int(input("Enter another number:  "))
+gcd = 0
+if p>i:
+    gcd = p%i 
+    while gcd!=0:
+        gcd  = p%i
+        p = i
+        i = gcd
+elif p<i:
+    gcd = i%p
+    while gcd!=0:
+        gcd  = i%p
+        i = p
+        p = gcd
+
+else:
+    print("The GCD: 1")
+
+print("The GCD is :  ",gcd)
